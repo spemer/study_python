a62b0a9618fd4420e027a4ffa9217e594f80f917
Hyouk Seo <ghsspower@naver.com>
2017-06-28 09:54:21 +0900

    class

--- summer2017/first/classFirst.py
+++ summer2017/first/classFirst.py
@@ -5,9 +5,18 @@ _c.v1 = int(input("Numb 1: "))
 _c.v2 = int(input("numb 2: "))
 print()
 
+########## numb ##########
+
+print("Numb 1: %d" % _c.getValue())
+print("Numb 2: %d" % _c._getValue())
+print()
+
+_c.setValue(_c.v1)
+_c.setValue(_c.v2)
+
+########## calc ##########
 
 print("%d + %d = %d" % (_c.v1, _c.v2, _c.add() ))
 print("%d - %d = %d" % (_c.v1, _c.v2, _c.sub() ))
 print("%d * %d = %d" % (_c.v1, _c.v2, _c.mul() ))
 print("%d / %d = %d" % (_c.v1, _c.v2, _c.div() ))
-print()
--- summer2017/first/classFirstFunc.py
+++ summer2017/first/classFirstFunc.py
@@ -1,7 +1,9 @@
 class _calc(object):
     def __init__(self, v1, v2):
-        self.v1 = v1
-        self.v2 = v2
+        self._v1 = v1
+        self._v2 = v2
+
+########## calc ##########
 
     def add(self):
         return self.v1 + self.v2
@@ -14,3 +16,17 @@ class _calc(object):
 
     def div(self):
         return self.v1 / self.v2
+
+########## numb ##########
+
+    def getValue(self):
+        return self.v1
+
+    def setValue(self, v1):
+        self._v1 = v1
+
+    def _getValue(self):
+        return self.v2
+
+    def _setValue(self, v2):
+        self._v2 = v2
